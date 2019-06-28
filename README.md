f267596ae72c8da50ff8c428bd44eb0ea70596b6
MinganMuon <mingan-muon@outlook.com>
2019-06-28 18:49:45 -0500

    Initial commit with basic main.cpp, readme, and gitignore.

---
+++ README.md
@@ -0,0 +1,5 @@
+# clara-v4
+
+Plays checkers.
+
+On Windows under MSVC, compile via ``cl.exe -std:c++17 -W4 -EHsc -Fe: clara.exe main.cpp''.
---
+++ main.cpp
@@ -0,0 +1,11 @@
+// main.cpp
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::cout << "clara-v4 - plays checkers\n";
+    std::cout << "-------------------------\n\n";
+
+    return 0;
+}
